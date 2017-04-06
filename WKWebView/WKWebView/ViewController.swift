9de125621b3e3c5f5d0625b3a8529791a336319a
Victor Chee <victor.qhj@gmail.com>
2017-04-06 09:01:46 +0800

    Migrate to Swift 3.

--- WKWebView/WKWebView/ViewController.swift
+++ WKWebView/WKWebView/ViewController.swift
@@ -19,60 +19,60 @@ class ViewController: UIViewController {
         webView.allowsBackForwardNavigationGestures = true
         webView.allowsLinkPreview = true
         webView.navigationDelegate = self;
-        webView.UIDelegate = self
+        webView.uiDelegate = self
         view.insertSubview(webView, belowSubview: progressView)
         
         // Layout
         webView.translatesAutoresizingMaskIntoConstraints = false
         let views: [String : AnyObject] = ["webView" : webView, "topLayoutGuide" : topLayoutGuide]
         var constraints = [NSLayoutConstraint]()
-        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[webView]|", options: [], metrics: nil, views: views)
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[webView]|", options: [], metrics: nil, views: views)
         constraints += horizontalConstraints
-        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[topLayoutGuide][webView]|", options: [], metrics: nil, views: views)
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[topLayoutGuide][webView]|", options: [], metrics: nil, views: views)
         constraints += verticalConstraints
-        NSLayoutConstraint.activateConstraints(constraints)
+        NSLayoutConstraint.activate(constraints)
         
-        webView.loadRequest(NSURLRequest(URL: NSURL(string: "https://victorchee.github.io")!))
+        webView.load(URLRequest(url: URL(string: "https://victorchee.github.io")!))
         
-        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
     }
     
-    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-        if let keyPath = keyPath where keyPath == "estimatedProgress" {
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if let keyPath = keyPath, keyPath == "estimatedProgress" {
             progressView.progress = change?["new"] as? Float ?? 0
         }
     }
 }
 
 extension ViewController: WKNavigationDelegate {
-    func webView(webView: WKWebView, didCommitNavigation navigation: WKNavigation!) {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         print("webView did commit navigation : \(navigation)")
     }
     
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("webView did finish navigation: \(navigation)")
     }
     
-    func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         print("webView did fail navigation: \(navigation) with error: \(error.localizedDescription)")
     }
     
-    func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         print("webView did start provisional navigation: \(navigation)")
     }
     
-    func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         print("webView did fail provisional navigation: \(navigation)")
     }
 }
 
 extension ViewController: WKUIDelegate {
-    func webView(webView: WKWebView, createWebViewWithConfiguration configuration: WKWebViewConfiguration, forNavigationAction navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
         print("webView create web view with configuration: \(configuration)")
         return webView
     }
     
-    func webViewDidClose(webView: WKWebView) {
+    func webViewDidClose(_ webView: WKWebView) {
         print("webView did close")
     }
 }
